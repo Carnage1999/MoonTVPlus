9cbe4b560452554180db7be909a2107fce521366
Han-che Wang <60660116+Carnage1999@users.noreply.github.com>
2026-03-13 09:50:59 +0800

    chore: migrate supported configs to typescript

--- .prettierrc.ts
+++ .prettierrc.ts
@@ -1,7 +1,11 @@
-module.exports = {
+import type { Config } from 'prettier';
+
+const config: Config = {
   arrowParens: 'always',
   singleQuote: true,
   jsxSingleQuote: true,
   tabWidth: 2,
   semi: true,
 };
+
+export default config;
---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^~/(.*)$': '<rootDir>/public/$1',
+    '^.+\\.(svg)$': '<rootDir>/src/__mocks__/svg.tsx',
+  },
+};
+
+export default createJestConfig(customJestConfig);
